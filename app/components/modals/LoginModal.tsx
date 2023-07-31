c94b444bacca67b7a88a3c6a3a7919b323af34b3
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-07-31 18:19:48 -0400

    implementa signUp con github

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -90,7 +90,7 @@ const LoginModal = () => {
         outline
         label="Continue with github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className=" text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row items-center gap-2 justify-center">
