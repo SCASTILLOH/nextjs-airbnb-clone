7f072528ec9a96dbf008bfccd72d837a26a617d0
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-02 13:25:54 -0400

    implementa Calendar y totalPrice en reservations

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Range } from "react-date-range";
 import Calendar from "../inputs/Calendar";
+import Button from "../Button";
 
 interface ListingReservationProps {
   price: number;
@@ -47,6 +48,9 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
         onChange={(value) => onChangeDate(value.selection)}
       />
       <hr />
+      <div className="p-4">
+        <Button disabled={disabled} label="Reserve" onClick={onSubmit} />
+      </div>
       <div
         className="
         p-4 flex flex-row items-center justify-between font-semibold text-lg
