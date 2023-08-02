21bccd32406530a40585152b0e91f35709762507
SCASTILLOH <sebastian.castillo.83z@gmail.com>
2023-08-02 09:18:21 -0400

    crea SafeListing types

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -2,7 +2,7 @@
 import useCountries from "@/app/hooks/useCountries";
 import React, { useCallback, useMemo } from "react";
 
-import { SafeUser } from "@/app/types";
+import { SafeUser, SafeListing } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
@@ -11,7 +11,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
